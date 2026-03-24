a3a284b7a9c0f366b8cb96148c6a99fcd810dcf2
Franco Vilcahuaman <74285297@continental.edu.pe>
2026-03-23 21:24:56 -0500

    Commit nuevo de nuevo

--- index.php
+++ index.php
@@ -3,6 +3,4 @@ echo "<h1>Bienvenido a mi web - Franco Vilcahuaman</h1>";
 echo "<p>Selecciona una opción:</p>";
 echo "<a href='suma.php'>Ir a suma</a><br>";
 echo "<a href='factorial.php'>Ir a factorial</a>";
-echo "<a href='potencia.php'>Ir a potencia</a><br>";
-echo "<a href='fibonacci.php'>Ir a fibonacci</a><br>";
 ?>
