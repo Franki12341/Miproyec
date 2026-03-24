f92c8737414fa0a3df5f6c8e1697150bef2f91fa
Franco Vilcahuaman <74285297@continental.edu.pe>
2026-03-23 21:34:12 -0500

    Ma palabras de commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "<h1>Bienvenido a mi web - Franco Vilcahuaman</h1>";
+echo "<p>Esta es una página de inicio que te permite acceder a diferentes funcionalidades.</p>";
 echo "<p>Selecciona una opción:</p>";
 echo "<a href='suma.php'>Ir a suma</a><br>";
 echo "<a href='factorial.php'>Ir a factorial</a>";
